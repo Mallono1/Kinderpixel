fc689c5e4d11a82fe2a82b207b5ab109c065792e
Marian Bak <mlazik1@gmail.com>
2025-02-04 23:15:30 +0100

    SidebarNavi for mobile - responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/img/hand-logo2.png";
 import { Link } from "react-scroll";
+import menu_icon from "../../assets/img/SidebarNavi.png";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -15,9 +16,14 @@ const Navbar = () => {
     });
   }, []);
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
+
   return (
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
-      <ul>
+      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>
           <Link to="home" smooth={true} offset={0} duration={500}>
             Home
@@ -50,6 +56,7 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
+      <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
       <img src={logo} alt="" className="logo" />
     </nav>
   );
