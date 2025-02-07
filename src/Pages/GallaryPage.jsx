d89232d96878d75cee43b3c292589df0e252b92b
Marian Bak <mlazik1@gmail.com>
2025-02-07 14:14:03 +0100

    GallaryPage where the Api should be implemented

---
+++ src/Pages/GallaryPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./GallaryPage.css";
+
+function GallaryPage() {
+  return (
+    <div>
+      <h1>GallaryPage</h1>
+    </div>
+  );
+}
+
+export default GallaryPage;
