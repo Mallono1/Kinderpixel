ca02b82614ac0e2248d2dc71e32a2d87e5115f58
Marian Bak <mlazik1@gmail.com>
2025-02-15 00:27:03 +0100

    RatingPage first step for the mobile version

--- src/Pages/RatingPage.jsx
+++ src/Pages/RatingPage.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import './RatingPage.css'
+import React from "react";
+import "./RatingPage.css";
 import rating_1 from "../assets/img/gallery-1.jpg";
 import rating_2 from "../assets/img/gallery-2.jpg";
 import rating_3 from "../assets/img/gallery-3.jpg";
 import rating_4 from "../assets/img/gallery-4.jpg";
+import DiscoButton from "../Components/RatingButton";
 
 function RatingPage() {
   return (
-     <div className="rating">
-          <div className="ratingFotos">
-            <img src={rating_1} alt="" />
-            <img src={rating_2} alt="" />
-            <img src={rating_3} alt="" />
-            <img src={rating_4} alt="" />
-          </div>
-          <button className="btn">See more here</button>
-        </div>
-  )
+    <div className="rating">
+      <div className="ratingFotos">
+        <img src={rating_1} alt="" />
+        <br />
+        <DiscoButton />
+        <img src={rating_2} alt="" />
+        <DiscoButton />
+        <img src={rating_3} alt="" />
+        <DiscoButton />
+        <img src={rating_4} alt="" />
+        <DiscoButton />
+      </div>
+    </div>
+  );
 }
 
 export default RatingPage;
